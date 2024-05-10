78dd0d6d211ceffc4fc39a9a2fb1cce795325735
flagarde <flagarde.programs@pm.me>
2024-05-11 01:08:48 +0800

    Update C.h

--- include/Khaos/C.h
+++ include/Khaos/C.h
@@ -18,9 +18,9 @@
 
 #if defined(__STDC__)
   #if defined(__STDC_VERSION__)
-  #define KHAOS_C_STANDARD_PRIVATE() (__STDC_VERSION__) /*!< defined as __STDC_VERSION__ */
+    #define KHAOS_C_STANDARD_PRIVATE() (__STDC_VERSION__) /* //NOSONAR */ /*!< defined as __STDC_VERSION__ */
   #else
-  #define KHAOS_C_STANDARD_PRIVATE() KHAOS_STANDARD_C90_PRIVATE() /*!< defined as C90 */
+    #define KHAOS_C_STANDARD_PRIVATE() KHAOS_STANDARD_C90_PRIVATE() /* //NOSONAR */ /*!< defined as C90 */
   #endif
 #else
   /*
@@ -34,19 +34,19 @@
     * The compiler treats Microsoft extended keywords as simple identifiers, disables the other Microsoft extensions, and automatically defines the __STDC__ predefined macro for C programs.
     */
     #if defined(__STDC_VERSION__)
-      #define KHAOS_C_STANDARD_PRIVATE() (__STDC_VERSION__) /*!< defined as __STDC_VERSION__ */
+      #define KHAOS_C_STANDARD_PRIVATE() (__STDC_VERSION__) /* //NOSONAR */ /*!< defined as __STDC_VERSION__ */
     #else
-      #define KHAOS_C_STANDARD_PRIVATE() KHAOS_STANDARD_C90_PRIVATE() /*!< defined as C90 */
+      #define KHAOS_C_STANDARD_PRIVATE() KHAOS_STANDARD_C90_PRIVATE() /* //NOSONAR */ /*!< defined as C90 */
     #endif
   #endif
 #endif
 
 #if defined(__STRICT_ANSI__)
-  #define KHAOS_LANGUAGE_ISO_PRIVATE()        (1L) /*!< 1 if strict ISO, 0 otherwise */
-  #define KHAOS_LANGUAGE_Extensions_PRIVATE() (0L) /*!< 0 if strict ISO, 1 otherwise */
+  #define KHAOS_LANGUAGE_ISO_PRIVATE()        (1L) /* //NOSONAR */ /*!< 1 if strict ISO, 0 otherwise */
+  #define KHAOS_LANGUAGE_Extensions_PRIVATE() (0L) /* //NOSONAR */ /*!< 0 if strict ISO, 1 otherwise */
 #else
-  #define KHAOS_LANGUAGE_ISO_PRIVATE()        (0L) /*!< 1 if strict ISO, 0 otherwise */
-  #define KHAOS_LANGUAGE_Extensions_PRIVATE() (1L) /*!< 0 if strict ISO, 1 otherwise */
+  #define KHAOS_LANGUAGE_ISO_PRIVATE()        (0L) /* //NOSONAR */ /*!< 1 if strict ISO, 0 otherwise */
+  #define KHAOS_LANGUAGE_Extensions_PRIVATE() (1L) /* //NOSONAR */ /*!< 0 if strict ISO, 1 otherwise */
 #endif
 
 #endif /* KHAOS_C_H_ */
