8dd925affdffc20701e25b0d0682b10c4e14b2df
flagarde <flagarde.programs@pm.me>
2024-05-14 13:01:56 +0800

    fix

--- include/Khaos/Compiler.h
+++ include/Khaos/Compiler.h
@@ -1100,7 +1100,7 @@
   #define KHAOS_DEFINE_Compiler_VERSION_PRIVATE() SET_VERSION(__PATHCC__, __PATHCC_MINOR__, __PATHCC_PATCHLEVEL__, 0)
   #define KHAOS_COMPILER_EKOPath_PRIVATE()        (1L)
 #elif defined(__EMSCRIPTEN__)
-  if !defined(__EMSCRIPTEN_major__) || !defined(__EMSCRIPTEN_minor__) || !defined(__EMSCRIPTEN_tiny__)
+  #if !defined(__EMSCRIPTEN_major__) || !defined(__EMSCRIPTEN_minor__) || !defined(__EMSCRIPTEN_tiny__)
     #include<emscripten/version.h>
   #endif
   #define KHAOS_DEFINE_Compiler_VERSION_PRIVATE() SET_VERSION(__EMSCRIPTEN_major__, __EMSCRIPTEN_minor__, __EMSCRIPTEN_tiny__, 0)
